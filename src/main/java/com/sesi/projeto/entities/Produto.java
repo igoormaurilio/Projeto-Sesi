ed31aa0dcc2afd5e94bd0ed5318a3dc57fac47d8
GustavoGomes-dev <gustavo10.ggj@gmail.com>
2025-01-28 11:46:02 -0300

    :lipstick: feat: implementação da entidade produto e seus endpoints

---
+++ src/main/java/com/sesi/projeto/entities/Produto.java
@@ -0,0 +1,58 @@
+package com.sesi.projeto.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+	private double preco;
+	private String descricao;
+	
+	
+	public Produto() {
+		super();
+	}
+	public Produto(long id, String nome, double preco, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
